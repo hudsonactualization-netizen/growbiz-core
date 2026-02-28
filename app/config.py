e50e0116fd2b07af7e7b33ee8f109acdc0307871
hudsonactualization-netizen <hudsonactualguruization@gmail.com>
2026-02-28 12:53:15 +0300

    Fix SQLALCHEMY_DATABASE_URI environment variable

--- app/config.py
+++ app/config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "devkey")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
